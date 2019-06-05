f2eb7749ae0af106c831b3d6112527c574d60b5f
Alex <iserjude@yahoo.com>
2019-06-05 22:26:42 +0300

    adding files

---
+++ Live.py
@@ -0,0 +1,54 @@
+import MemoryGame
+import GuessGame
+import CurrencyRouletteGame
+
+# Function to welcome a player. It gets name and prints greeting
+def welcome(name) :
+    return "\nHello " + name + " and welcome to the world of Games (WoG). \nHere you can find many cool games to play\n"
+
+
+# Function to get a player to choose which play he will play and what difficulty level it will be.
+def load_game() :
+    no_exception = False                     # Flag to keep info whether exception was thrown or not
+    while no_exception == False:
+        print("Please choose a game to play: \n"
+              "\t1. Memory Game - a sequence of  numbers  will appear for 1 second and you have to guess it back \n"
+              "\t2. Guess Game - guess a number and see if you  chose like the computer \n"
+              "\t3. Currency  Roulette - try and guess the value of a random amount of USD in ILS")
+        try:
+             user__game_choice = int(input(">>>"))
+             if user__game_choice not in range (1,4):
+                raise ValueError
+             else:
+                 no_exception = True        # The exception was not thrown, so the loop will be finished.
+        except ValueError as e:
+                print("\t\t!!!!!!!!!!!!!!!   Numbers 1, 2, 3 are only allowed   !!!!!!!!!!!!!!!!!\n")
+
+    no_exception = False                    # Flag to keep info whether exception was thrown or not
+    while no_exception == False:
+        print("Please choose game difficulty from 1 to 5:")
+        try:
+            user__dificulty_choice = int(input(">>>"))
+            if user__dificulty_choice not in range (1,6):
+                raise ValueError
+            else:
+                no_exception = True         # The exception was not thrown, so the loop will be finished.
+        except ValueError as e:
+                print("\t\t!!!!!!!!!!!!!!!   Numbers from 1 to 5 are only allowed   !!!!!!!!!!!!!!!!!\n")
+
+    if user__game_choice == 2:
+        GuessGame.difficulty = user__dificulty_choice
+        result = GuessGame.play()
+    elif user__game_choice == 1:
+        MemoryGame.difficulty = user__dificulty_choice
+        result = MemoryGame.play()
+    elif user__game_choice == 3:
+        CurrencyRouletteGame.difficulty = user__dificulty_choice
+        result = CurrencyRouletteGame.play()
+
+    if result == True:
+        print("                                     You won")
+    else:
+        print("                                     You lost")
+
+
---
+++ MemoryGame.py
@@ -0,0 +1,52 @@
+import random
+from time import sleep
+import os
+import sys
+
+difficulty = 3
+list = []
+
+def generate_sequence():
+    global list
+    for index in range(0, difficulty):
+       list.append(random.randint(1, 101))
+
+
+def get_list_from_user():
+    guess_list = []
+    for index in range(1, difficulty + 1):
+        no_exception = False  # Flag to keep info whether exception was thrown or not
+        while no_exception == False:
+            print("Please type your guess number by number - " + str(index) +  " number: ")
+            try:
+                user__guess = int(input(">>>"))
+                if (user__guess) not in range(1, 101):
+                    raise ValueError
+                else:
+                    no_exception = True  # The exception was not thrown, so the loop will be finished.
+            except ValueError as e:
+                print("\t\t!!!!!!!!!!!!!!!   Numbers between 1 to 100 are only allowed   !!!!!!!!!!!!!!!!!\n")
+        guess_list.append(user__guess)
+    return guess_list
+
+
+
+def is_list_equal():
+    list1 = set(list)
+    print("You chose memory Game. Try to memorize the following set of numbers (you have only 0.9 sec.): ")
+    print((list1))
+    sleep(0.9)
+    #os.system("cls")                  # Works only in Windows shell
+    print("\n"*100)                   # Clears screen in Pycharm screen
+    list2 = set(get_list_from_user())
+    print("System casted numbers: " + str(list1) + ", you entered numbers : " + str(list2) + "." )
+    if len(list1 & list2) == difficulty:
+        return True
+    return False
+
+
+def play():
+    generate_sequence()
+    return is_list_equal()
+
+
